de13400bb950d33bd650fe3c0794099d4ae0e6ce
Jens Joy <jensjoy54@gmail.com>
2023-03-10 13:50:08 +0530

    redesign

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -2,32 +2,52 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import './css/Login.scss';
+import {TbWritingSign} from "react-icons/tb";
+
+import {AiFillEye,AiFillEyeInvisible} from "react-icons/ai";
+// import {TextInput} from 'react-native';
 
 
+    function Login() {
+      
+       
+        
+       
+        const [passvisible,setPassVisible]=React.useState(false);
+        const togglePass=()=>
+        {
+            setPassVisible(!passvisible);
+        };
 
-class Login extends React.Component
-{
-    render()
-    {
         return(
-            <div className="app">
-                <div>
-                <div>EmailAddress</div>
-                <input type="text" placeholder="Email Address"></input>
-                <div>Password</div>
-                <input type="password" placeholder="Password"></input>
-                <div>Forgot your password?</div>
-                <button><Link to={"/Home"}>
+            <div className="app" style={{height:"100vh"}}>
+                
+                    
+            <div style={{maxWidth:"500px",maxHeight:'100px'}}>
+            <div id="loginbtnanime" style={{fontWeight:"bold",fontSize:"35px",display:"flex",alignItems:"center",marginTop:"200px",marginLeft:"50px"}}>To continue, log in to Feelchords<TbWritingSign/></div>
+            <div style={{position:"absolute",right:"100px",top:"100px"}}> 
+                <div id="displayquestions" >EmailAddress</div>
+                <input id="formbox" type="text" placeholder="Email Address" style={{width:"300px"}}></input>
+                <div id="displayquestions">Password</div>
+                
+                <div style={{display:"flex",marginLeft:"30px"}}> <input type={passvisible ? "text" : "password"} required  id="formbox" style={{left:"0px",width:"250px",border:"0.5px solid lightgray",borderRight:"none"}}  placeholder="Enter your Password"    /> 
+    <div style={{marginTop:"20px",cursor:"pointer"}}   onClick={togglePass}>{passvisible ? <AiFillEye/> : <AiFillEyeInvisible/>}</div>  </div>
+                
+                <div id="displayquestions">Forgot your password?</div>
+                <button id="btn" style={{marginTop:"30px"}}><Link to={"/Home"} style={{color:"black"}}>
                         LOG IN </Link></button>
-                <div>Don't have an account?</div>
-                <div><Link to={"/SignUp"} className="btn" style={{marginLeft:"60px"}}>
+                <div style={{marginTop:"10px"}}>Don't have an account?</div>
+                <div style={{marginTop:"10px"}}><Link to={"/SignUp"}  >
                         Sign Up
                     </Link> 
                 </div>
                 </div>
+                </div>
             </div>
+            
+            
         )
     }
-}
+
 
 export default Login;
--- src/components/Pages/SignUp.jsx
+++ src/components/Pages/SignUp.jsx
@@ -2,43 +2,57 @@
 import React from "react";
 import './css/SignUp.scss';
 import {Link} from "react-router-dom";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
 
-class SignUp extends React.Component
-{
-    render()
+import {AiFillEye,AiFillEyeInvisible} from "react-icons/ai";
+
+    function SignUp()
     {
-        return(
-            <div className="app">
+        
+       
+        
+        
+        const [passvisible,setPassVisible]=React.useState(false);
+        const togglePass=()=>
+        {
+            setPassVisible(!passvisible);
+        };
 
-            <div style={{maxWidth:"500px",maxHeight:'100px'}}>
-                <div style={{fontWeight:"bold",textAlign:"center",fontSize:"35px",paddingTop:"30px",paddingBottom:"40px"}}>Sign up for free to start listening.</div>
-                <div id="displayquestions">Enter your email</div>
-                <input type="text" placeholder="Enter your E-mail"></input>
+        return(
+            <div className="app" style={{maxHeight:"120vh"}}>
+            
+            <div style={{maxWidth:"1000px",maxHeight:'100px'}}>
+                <div id="btn-shine" style={{fontWeight:"bold",fontSize:"35px",display:"flex",alignItems:"center",marginTop:"150px",marginLeft:"50px"}}>Sign up for free to <br/><p style={{marginTop:"70px"}}>start listening.</p></div>
+              <div style={{position:"absolute",right:"30px",top:"20px"}}> 
+              <div id="displayquestions">Enter your email</div>
+                <input type="text" placeholder="Enter your E-mail" id="formbox"></input>
                 <div id="displayquestions">Enter your password</div>
-                <input type="text" placeholder="Create a password"></input>
-                <div id="displayquestions">confirm your password</div>
-                <input type="text" placeholder="password"></input>
+ <div style={{display:"flex",marginLeft:"100px"}}> <input type={passvisible ? "text" : "password"} required  id="formbox" style={{left:"0px",width:"300px",border:"0.5px solid lightgray",borderRight:"none"}}  placeholder="Enter your Password"    /> 
+    <div style={{marginTop:"20px",paddingLeft:"-40px",cursor:"pointer"}}   onClick={togglePass}>{passvisible ? <AiFillEye/> : <AiFillEyeInvisible/>}</div>  
+    </div>
+                <div id="displayquestions">Confirm your password</div>
+                <div style={{display:"flex",marginLeft:"100px"}}> <input type={passvisible ? "text" : "password"} required  id="formbox" style={{left:"0px",width:"300px",border:"0.5px solid lightgray",borderRight:"none"}}  placeholder="Confirm your Password"    /> 
+    <div style={{marginTop:"20px",paddingLeft:"-40px",cursor:"pointer"}}   onClick={togglePass}>{passvisible ? <AiFillEye/> : <AiFillEyeInvisible/>}</div>  
+    </div>
                 <div id="displayquestions">Enter your Feelchords ID</div>
-                <input type="text" placeholder="Enter a profile name."></input>
+                <input type="text" placeholder="Enter a profile name." id="formbox"></input>
                 <div id="displayquestions">What's your date of birth?</div>
                 <div id="">
-                    <input type="date"></input> 
+                    <input type="date" style={{width:"30%",borderRadius:"10px",padding:"8px"}}></input> 
                 </div>
 
-                <div id="displayquestions">What's your gender?</div>
-               <div> <input type="radio" />Male                
-                <input type="radio"/>Female
-                <input type="radio"/>Other
-                <input type="radio"/>Prefer not to say
+                <div id="displayquestions" style={{marginBottom:"10px"}}>What's your gender?</div>
+               <div> <input type="radio" id="radiobutton"/>Male
+                <input type="radio" id="radiobutton"/>Female
+                <input type="radio" id="radiobutton"/>Other
+                <input type="radio" id="radiobutton"/>Prefer not to say
                 </div>
-                <div classname ="bn">
-                <button>Sign up</button></div>
+                <div >
+                <button id="btn">Sign up</button></div>
                 <div>
             <h5>or</h5>
             <br />
-            <GoogleLogin 
-            buttonText="Sign In with Google"/>
+                {/* <GoogleLogin 
+                buttonText="Sign In with Google"/> */}
             </div>
 
                 <div>Have an account?</div>
@@ -46,14 +60,16 @@ class SignUp extends React.Component
                         Log in
                     </Link> 
                 </div>
+
+                </div> 
             </div>
 
-            </div>
+             </div>
         );
     }
     
 
     
 
-}
+
 export default SignUp;
